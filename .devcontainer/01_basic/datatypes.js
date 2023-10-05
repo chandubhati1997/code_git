9b53091d51943189bc77545240d315c00a08d4f4
chandubhati1997 <104966485+chandubhati1997@users.noreply.github.com>
2023-10-05 09:56:50 +0000

    datatypes

--- .devcontainer/01_basic/datatypes.js
+++ .devcontainer/01_basic/datatypes.js
@@ -4,5 +4,38 @@
 //alert("hello")  /// we are using nodejs and not browser so we cannot use alert 
 
 
-console.log(3+3); 
-lo
+console.log(3
+    +                      //code readibilty              
+     3); 
+
+
+
+ let name = "chandu"
+ 
+ let age =18
+
+ let isloggedin =false
+
+
+ let state =null
+
+
+ //number =>2 to power 53
+
+ //bigint
+
+ //string => " "
+
+ //boolean => true/false
+
+
+ //null => standalone value  
+
+ //undefined
+
+ //symbol => unique
+
+ //object 
+
+
+ console.log(typeof undefined);
