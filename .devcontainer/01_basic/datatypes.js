e969e5c389ca9d06f4811a71d45414a1dc563a7b
chandubhati1997 <104966485+chandubhati1997@users.noreply.github.com>
2023-10-05 09:47:47 +0000

    added file

---
+++ .devcontainer/01_basic/datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all JS code as newer version
+
+
+//alert("hello")  /// we are using nodejs and not browser so we cannot use alert 
+
+
+console.log(3+3); 
+lo
---
+++ .devcontainer/01_basic/variables.js
@@ -0,0 +1,35 @@
+
+//not changeable
+const accountId =144553
+
+
+let accountEmail = "chandreveersingh@gmail.com"
+
+var accountPassword ="12345"
+
+accountCity ="Ahmedabad"
+
+let accountState;
+
+//accountId = 2
+
+accountEmail = "chandreveersingh"
+
+
+accountPassword ="212121"
+
+accountCity ="jaipur"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+prefer not to use var because of issue in block scope and fuctional scope....
+
+
+*/
