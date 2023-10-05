22940e3b137ba1b165fb607413c046a65d87c216
chandubhati1997 <104966485+chandubhati1997@users.noreply.github.com>
2023-10-05 11:03:46 +0000

    conversion_operation

---
+++ .devcontainer/01_basic/conversion_operation.js
@@ -0,0 +1,23 @@
+ //let score = "chandu"
+
+ //console.log(typeof score);
+
+ //let valueInNumber = Number(score)  ///number 
+
+ //console.log(valueInNumber);   ///
+
+
+
+ //"33" => 33
+ //"33abc" = Nan
+ //"true" => 1 "false" => 0
+
+
+ let isloggedIn ="c";
+
+ let boolenisloggedIn =Boolean(isloggedIn)
+ console.log( boolenisloggedIn);
+
+
+ let someNumber =33
+ 
