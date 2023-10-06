f90886b64048845896cf35ff828355236e73324d
chanderveer singh bhati <104966485+chandubhati1997@users.noreply.github.com>
2023-10-06 03:32:44 +0000

    c_op added

--- .devcontainer/01_basic/conversion_operation.js
+++ .devcontainer/01_basic/conversion_operation.js
@@ -16,8 +16,42 @@
  let isloggedIn ="c";
 
  let boolenisloggedIn =Boolean(isloggedIn)
- console.log( boolenisloggedIn);
+ //console.log( boolenisloggedIn);
 
 
  let someNumber =33
+
+
+ //***************operations*******//
+
+
+ let value =3 
+ let negvalue = -value;
+
+ console.log(negvalue);
+
+ //console.log(2+2);
+ //console.log(2-2);
  
+ //console.log(2*2);
+// console.log(2/2);
+ //console.log(2**2);
+// console.log(3%2);
+
+
+let str1 ="hello"
+
+let str2 =" chandu"
+
+let str3 = str1+str2
+
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+
+console.log("1"+2+2);
+
+console.log(1+2 +"2");
+
