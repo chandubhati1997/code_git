5af76df7756f328973d25d52bf54de3346a34878
chanderveer singh bhati <104966485+chandubhati1997@users.noreply.github.com>
2023-10-11 06:57:05 +0000

    array changes

---
+++ .devcontainer/ds/array.js
@@ -0,0 +1,35 @@
+//array 
+const myArr = ['a','b','c','d','e']
+
+myArr.push(7)
+console.log(myArr)
+myArr.pop();
+console.log(myArr)
+
+
+const myarr2 = new Array(1,2,3,4)
+const heros =["shaktiman","naagraj"]
+
+console.log(myarr2);
+
+console.log(myarr2.at(2)) //helps in 
+
+console.log(myArr.concat(myarr2));// concating the array
+
+
+//console.log(myArr.copyWithin(0,3,5));
+
+
+
+const it =myArr.entries()  // array entries iterater which key value pair in array 
+
+
+console.log(it.next().value);
+console.log(it.next().value);
+
+console.log(it);
+
+
+
+
+
