492ac8bcfccb2d2604c838be0c4adc830284c5b0
chanderveer singh bhati <104966485+chandubhati1997@users.noreply.github.com>
2023-10-13 09:41:30 +0000

    object created

---
+++ .devcontainer/ds/objects.js
@@ -0,0 +1,23 @@
+// null is object but does not inherhit the prototype of objects or ojects methods 
+// singleton objects when you made it by constructor 
+// objects literal 
+
+// constructor thrugh making of object 
+ // by using create 
+// Object.create 
+
+//objectliterals
+
+const objuser ={
+
+    name: "chandreveer", //name is the key
+    age: 23,
+    location: "Ahmedabad",
+    email: "chandreveersingh.com",
+    isloggedin : false,
+    lastdayloggedin :["Mon","tue"]
+    
+}
+
+
+myArray =[]
